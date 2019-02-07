da4ad6fd2dade99e941b296552384e9de02821b7
Matthieu PETIT <m4tth.p@gmail.com>
2019-02-07 20:24:53 +0100

    Show an icon to upload a media instead of text

--- client/src/components/Topic/TopicDialog.vue
+++ client/src/components/Topic/TopicDialog.vue
@@ -242,11 +242,9 @@ export default {
   background-position: center center;
   background-image: url("\
   data:image/svg+xml;utf8, \
-    <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='170px' height='50px'> \
-      <text x='85' y='28' \
-        style='text-anchor: middle' font-size='16'> \
-        Click here to upload \
-      </text> \
+    <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='48px' height='48px' viewBox='0 0 24 24'> \
+      <path d='M0 0h24v24H0z' fill='none'/>\
+      <path d='M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z'/>\
     </svg>\
   ");
 }
